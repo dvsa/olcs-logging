3cbd268d3f02741a89ebad381a2d42eee67c4988
Rob Caiger <rob@clocal.co.uk>
2015-11-23 08:39:04 +0000

    Fixed exception logging

--- src/Log/Formatter/Exception.php
+++ src/Log/Formatter/Exception.php
@@ -14,7 +14,44 @@ class Exception extends AbstractFormatter
      */
     public function format($event)
     {
-        $exception = isset($event['extra']['exception']) ? $event['extra']['exception'] : new \Exception();
+        if (isset($event['extra']['exception'])) {
+            return $this->formatExtraException($event);
+        }
+
+        return $this->formatException($event);
+    }
+
+    protected function formatException($event)
+    {
+        $data = isset($event['extra']['data']) ? $event['extra']['data'] : [];
+        $data['remoteIp'] = $event['extra']['remoteIp'];
+        $event['extra']['data'] = $data;
+
+        $event = parent::format($event);
+        //need to improve this, currently the zend log error handler doesn't capture all the info we need...
+        return sprintf(
+            "^^*%s.%d||%d||%s||%s||%s||%s||%s||%s:%d||%s||%s||%s||%s||\n%s",
+            $this->getTimestamp($event),
+            $event['microsecs'],
+            $event['priority'],
+            $event['priorityName'],
+            isset($event['extra']['type']) ? $event['extra']['type'] : '',
+            $event['extra']['userId'],
+            $event['extra']['sessionId'],
+            $event['extra']['requestId'],
+            $event['extra']['file'],
+            $event['extra']['line'],
+            'Unknown',
+            $event['priority'],
+            $event['message'],
+            isset($event['extra']['data']) ? $event['extra']['data'] : '',
+            $event['extra']['trace']
+        );
+    }
+
+    protected function formatExtraException($event)
+    {
+        $exception = $event['extra']['exception'];
         $data = isset($event['extra']['data']) ? $event['extra']['data'] : [];
         $data['remoteIp'] = $event['extra']['remoteIp'];
         $event['extra']['data'] = $data;
