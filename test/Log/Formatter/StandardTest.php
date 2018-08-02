6606285ae69adacb85c31fc85b9093811e79274d
Parth <parth.vyas@dvsa.gov.uk>
2018-08-02 15:55:11 +0100

    Remove typo. ZF3 upgrade. OLCS-21043

--- test/Log/Formatter/StandardTest.php
+++ test/Log/Formatter/StandardTest.php
@@ -61,6 +61,6 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 
         $sut = new Standard();
         $actual = $sut->format($event);
-        $this->assertContains('TEST EXCEPTION', $actual)uy7t ;
+        $this->assertContains('TEST EXCEPTION', $actual);
     }
 }
